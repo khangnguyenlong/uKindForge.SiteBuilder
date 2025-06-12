4e5bfac9b907be20942bcedbb25b2c32ee5cb870
Khang Nguyen <khangnguyenlong@gmail.com>
2025-06-12 11:15:30 +0700

    restructure global settings page

---
+++ uKindForge.SiteBuilder.Core/Services/PageService.cs
@@ -0,0 +1,20 @@
+﻿using uKindForge.SiteBuilder.Models;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Extensions;
+
+namespace uKindForge.SiteBuilder.Core.Services
+{
+    public class PageService(IUmbracoContextFactory contextFactory) : IPageService
+    {
+        private readonly IUmbracoContextFactory _contextFactory = contextFactory;
+
+        public HomePage GetRootHomePage()
+        {
+            using var cref = _contextFactory.EnsureUmbracoContext();
+            var home = cref.UmbracoContext.Content.GetAtRoot()
+                    .FirstOrDefault(x => x.ContentType.Alias.InvariantEquals(HomePage.ModelTypeAlias)) as HomePage;
+
+            return home;
+        }
+    }
+}
